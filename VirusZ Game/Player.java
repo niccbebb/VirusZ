84bf2f75fb20f31dd4b6d81071eede81bff1804c
niccbebb <nicolecbebb@gmail.com>
2023-01-25 22:25:31 +0000

    Background and Player added.
    Player is able to turn towards the mouse.

---
+++ VirusZ Game/Player.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player extends Actor
+{
+    /**
+     * Act - do whatever the Player wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        MouseInfo pointer = Greenfoot.getMouseInfo();
+        if(pointer != null)
+        {
+            int mouseX = pointer.getX();
+            int mouseY = pointer.getY();
+            turnTowards(mouseX, mouseY);
+        }
+        // Add your action code here.
+    }
+}
